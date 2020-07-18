3427adbce82d1dc7616b220609db4df6a82d4207
jiangqianghua <240437339@qq.com>
2020-07-19 00:23:24 +0800

    完善todolist

--- src/TodoList.js
+++ src/TodoList.js
@@ -8,29 +8,69 @@ class TodoList extends Component {
     state = {  }
     constructor(props) {
         super(props)
-        console.log(store.getState())
         this.state = store.getState()
+        this.changeInputValue = this.changeInputValue.bind(this)
+        this.clickBtn = this.clickBtn.bind(this)
+        // 订阅store改变事件，把新的state设置到当前state
+        this.storeChange = this.storeChange.bind(this)
+        store.subscribe(this.storeChange)
     }
+
+    // 重新设置state
+    storeChange () {
+        console.log('storeChange')
+        this.setState(store.getState())
+    }
+
     render() { 
         return ( 
             <div style={{margin: '10px'}}>
                 <div>
                     <Input 
-                    placeholder={this.state.inputValue}
-                    style={{width: '250px', marginRight: '10px'}} />
+                    style={{width: '250px', marginRight: '10px'}} 
+                    onChange={this.changeInputValue}
+                    value={this.state.inputValue}
+                    />
 
-                    <Button type="primary" > 增加</Button>
+                    <Button type="primary" onClick={this.clickBtn} > 增加</Button>
                 </div>
-
+                <div>
+                    {this.state.inputValue}
+                </div>
                 <div style={{margin: '10px', width: '300px'}}>
                     <List
                         bordered
                         dataSource={this.state.list}
-        renderItem={(item) => (<List.Item>{item}</List.Item>)}
+                        renderItem={(item, index) => (<List.Item 
+                            onClick={this.deleteItem.bind(this, index)}>{item}</List.Item>)}
                     ></List>
                 </div>
             </div>
          );
+    }
+
+    changeInputValue(e) {
+        console.log(e.target.value)
+        const action = {
+            type: 'changeInput',
+            value: e.target.value
+        }
+        store.dispatch(action)
+    }
+
+    clickBtn () {
+        const action = {
+            type: 'addItem'
+        }
+        store.dispatch(action)
+    }
+
+    deleteItem(index) {
+        const action = {
+            type: 'deleteItem',
+            value: index
+        }
+        store.dispatch(action)
     }
 }
  
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -7,6 +7,26 @@ const defaultState = {
         '4444444444'
     ]
 }
+// 每次dispatch，都会执行该方法， action存放action的typoe和value
 export default (state = defaultState, action) => {
+    console.log(state, action)
+    // Reducer里只能接受state，只能改变state
+    if(action.type === 'changeInput') {
+        let newState = JSON.parse(JSON.stringify(state))
+        newState.inputValue = action.value
+        return newState
+    }
+    if (action.type === 'addItem') {
+        let newState = JSON.parse(JSON.stringify(state))
+        newState.list = [...newState.list, newState.inputValue]
+        newState.inputValue = ''
+        return newState
+    }
+    if (action.type === 'deleteItem') {
+        let newState = JSON.parse(JSON.stringify(state))
+        newState.list.splice(action.value, 1)
+        return newState
+    }
     return state
+ 
 }
