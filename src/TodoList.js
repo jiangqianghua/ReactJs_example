1c777e74806670de89f8636ead5ad4d02c403c9b
jiangqianghua <240437339@qq.com>
2020-07-18 22:29:49 +0800

    添加Antd

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,83 +1,35 @@
-import React, {Component, Fragment} from 'react'
-import TodoItem from './TodoItem'
-import { TransitionGroup, CSSTransition } from 'react-transition-group'
-
+import React, { Component } from 'react';
+import 'antd/dist/antd.css'
+import {Input, Button, List} from 'antd'
+const data = [
+    '11222333',
+    '33333333333',
+    '4444444444'
+]
 
 class TodoList extends Component {
-    constructor(pros) {
-        super(pros)
-        this.state = {
-            inputValue: '',
-            list: ['基础按摩', '精油推背']
-        }
-    }
-    render() {
-        return (
-            <Fragment>
+    state = {  }
+    render() { 
+        return ( 
+            <div style={{margin: '10px'}}>
                 <div>
-                    <label htmlFor="input_id">请输入文本</label>
-                    <input id="input_id" value={this.state.inputValue}
-                    onChange={this.inputChange.bind(this)}
-                    ref={(input) => {this.input=input}}  /> 
-                    <button onClick={this.addList.bind(this)}>增加服务器</button>
+                    <Input 
+                    placeholder="write something" 
+                    style={{width: '250px', marginRight: '10px'}} />
+
+                    <Button type="primary" > 增加</Button>
                 </div>
-                <ul ref={(ul) => {this.ul = ul}}>
-                    <TransitionGroup>
-                    {
-                        this.state.list.map((item, index) => {
-                            return (
-                                <CSSTransition
-                                timeout={2000}
-                                classNames='boss-text'
-                                unmountOnExit
-                                appear={true}
-                                key={index+item}
-                                >
-                                <TodoItem
-                                 content={item} 
-                                 index={index}
-                                 deleteItem={
-                                    this.deleteItem.bind(this)
-                                 }></TodoItem>
-                                 </CSSTransition>
-                            )
-                        })
-                    }
-                    </TransitionGroup>
-                </ul>
-            </Fragment>
-        )
-    }
 
-    inputChange(e) {
-        console.log(e.target.value)
-       this.setState({
-           inputValue: this.input.value
-       })
-    }
-
-    // 增加列表
-    addList() {
-        this.setState({
-                list: [...this.state.list, this.state.inputValue],
-                inputValue: ''
-            }, () => {
-                // 渲染完成后会执行该方法
-                console.log(this.ul.querySelectorAll("li").length)
-            })
-    }
-
-    deleteItem(index) {
-        // 一定要先定义局部变量在作删除
-        let list = this.state.list
-        list.splice(index, 1)
-        this.setState({
-            list: list
-        },() => {
-            // 渲染完成后会执行该方法
-            console.log(this.ul.querySelectorAll("li").length)
-        })
+                <div style={{margin: '10px', width: '300px'}}>
+                    <List
+                        bordered
+                        dataSource={data}
+        renderItem={(item) => (<List.Item>{item}</List.Item>)}
+                    ></List>
+                </div>
+            </div>
+         );
     }
 }
-
-export default TodoList
+ 
+export default TodoList;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-// import LifeCycle from './LifeCycle'
-// import TextAxios from './TextAxios'
-import TodoList from './TodoList'
-// import Anim from './Anim'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoList from './TodoList';;
 
 ReactDOM.render(
     <TodoList />,
     document.getElementById('root')
-)
+);
