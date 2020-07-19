be7af291ee095dd54c09391e1c128e4d62ddfdee
jiangqianghua <240437339@qq.com>
2020-07-19 17:08:14 +0800

    完成自定义hook函数

--- src/Example.js
+++ src/Example.js
@@ -1,27 +1,36 @@
-import React, { useRef, useState, useEffect } from 'react';
-// 获取元素对象
-function Example(){
-    const inputEl = useRef(null)
-    const onButtonClick = () => {
-        inputEl.current.value = "hello jiang"
-        console.log(inputEl)
-    }
-    const [text, setText] = useState('js')
-
-    const textRef = useRef()
-
-    // text每次改变，textRef都会执行一次保存
-    useEffect(() => {
-        textRef.current = text
-        console.log('textRef = ' + textRef.current)
+import React, { useCallback, useState, useEffect } from 'react';
+// 自定义hook
+function useWinSize() {
+    const [size, setSize] = useState({
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight
     })
-    return (
-        <>
-            <input ref={inputEl} type='text'></input>
-            <button onClick={onButtonClick}>在input上显示文字</button>
-            <input value={text} onChange={(e) => {setText(e.target.value)}} ></input>
-        </>
-    )
+
+    const onResize = useCallback(() => {
+        setSize({
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight
+        })
+    }, [])
+
+    useEffect(() => {
+        window.addEventListener('resize', onResize) 
+        return () => {
+            //  销毁移除监听
+            window.removeEventListener('resize', onResize)
+        }
+    }, []) // 最后的[]  可以确保销毁只执行一次
+    return size;
+}
+
+function Example(){
+   const size = useWinSize()
+
+   return (
+       <div>
+           页面size {size.width} : {size.height}
+       </div>
+   )
 }
 
 export default Example
