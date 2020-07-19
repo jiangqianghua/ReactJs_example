102797aa6fc9437d0d7ee3246a3710ee92c3775a
jiangqianghua <240437339@qq.com>
2020-07-19 16:47:37 +0800

    useRef相关

--- src/Example.js
+++ src/Example.js
@@ -1,28 +1,25 @@
-import React, { useState, useMemo } from 'react';
-
+import React, { useRef, useState, useEffect } from 'react';
+// 获取元素对象
 function Example(){
-    const [xiaohong, setXiaohong] = useState('小红待客状态')
-    const [zhiling,setZhiling] = useState('志林待客状态')
-    return (
-        <>
-            <button onClick={() => {setXiaohong(new Date().getTime())}}>小红</button>
-            <button onClick={() => {setZhiling(new Date().getTime() + '志玲走来了')}}>志玲</button>
-            <ChildComponent name={xiaohong}>{zhiling}</ChildComponent>
-        </>
-    )
-}
-
-function ChildComponent({name, children}) {
-    function changeXiaohong() {
-        console.log('他来了，小红向我们走来了')
-        return name + '小红向我们走来了'
+    const inputEl = useRef(null)
+    const onButtonClick = () => {
+        inputEl.current.value = "hello jiang"
+        console.log(inputEl)
     }
-    // 期望点击志玲的时候，小红状态不要变化
-    const  actionXIaohong = useMemo(()=>changeXiaohong(name), [name])
+    const [text, setText] = useState('js')
+
+    const textRef = useRef()
+
+    // text每次改变，textRef都会执行一次保存
+    useEffect(() => {
+        textRef.current = text
+        console.log('textRef = ' + textRef.current)
+    })
     return (
         <>
-        <div>{actionXIaohong}</div>
-        <div>{children}</div>
+            <input ref={inputEl} type='text'></input>
+            <button onClick={onButtonClick}>在input上显示文字</button>
+            <input value={text} onChange={(e) => {setText(e.target.value)}} ></input>
         </>
     )
 }
